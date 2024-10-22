168f2f0375978baccc3a468474495c64c2545df8
iabhinavrajput <abhinavrajput207@gmail.com>
2024-10-22 17:33:05 +0530

    uses the useEffect

--- src/screens/Dosa.jsx
+++ src/screens/Dosa.jsx
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 const Dosa = () => {
+  const [dosaInfo, setDosaInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchDosaInfo = async () => {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=dosa');
+      const data = await response.json();
+      setDosaInfo(data);
+    };
+
+    fetchDosaInfo();
+  }, []); 
+
   return (
     <View>
-      <Text>Dosa</Text>
+      <Text>Dosa Information</Text>
+      {dosaInfo ? (
+        <Text>{dosaInfo.description}</Text>  
+      ) : (
+        <Text>Loading...</Text>  
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default Dosa
+export default Dosa;
